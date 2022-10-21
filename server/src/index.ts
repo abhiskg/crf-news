b3a6b75fddc798fdf21cc9b0c579ebef0919845c
abhishek <abhiskg20@gmail.com>
2022-10-21 22:49:07 +0600

    Api routes created for category news

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
+import { newsDataType } from "./types/newsDataType";
 const categories = require("../data/categories.json");
+const news = require("../data/news.json") as newsDataType[];
 
 const app = express();
 
@@ -12,6 +14,23 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/news-categories", (req: Request, res: Response) => {
   res.send(categories);
+});
+
+app.get("/news-category/:id", (req: Request, res: Response) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const filteredNews = news.filter((n) => n.category_id === id);
+    res.send(filteredNews);
+  }
+});
+
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(5000, () => console.log("Server running"));
